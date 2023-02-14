5d2181a7830fd068edee28a08a603b5c8478634a
Bhushan Zade <bhushanzade.in@gmail.com>
2023-02-14 20:37:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -59,8 +59,7 @@ if st.button('Predict Price'):
     X_res = int(resolution.split('x')[0])
     Y_res = int(resolution.split('x')[1])
     ppi = ((X_res**2) + (Y_res**2))**0.5/screen_size
-    query = np.array([company,type,ram,weight,touchscreen,ips,ppi,cpu,hdd,ssd,gpu,os])
+    query = pd.DataFrame(np.array([[company, type, ram, weight, touchscreen, ips, screen_size, resolution, cpu, hdd, ssd, gpu, os]]), columns=['Company', 'TypeName', 'Ram', 'Weight', 'Touchscreen', 'Ips', 'ScreenSize', 'ScreenResolution', 'Cpu brand', 'HDD', 'SSD', 'Gpu brand', 'os'])
 
-    query = query.reshape(1,12)
     st.title("The predicted price of this configuration is " + str(int(np.exp(pipe.predict(query)[0]))))
 
