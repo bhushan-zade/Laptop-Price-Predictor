3b3e508d1c702aa6c39b2b33e9abef3006f0b261
Bhushan Zade <bhushanzade.in@gmail.com>
2023-02-14 21:59:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -122,7 +122,10 @@ if st.button('Predict Price'):
 
     X_res = int(resolution.split('x')[0])
     Y_res = int(resolution.split('x')[1])
-    ppi = ((X_res**2) + (Y_res**2))**0.5/screen_size
+    try:
+        ppi = ((X_res**2) + (Y_res**2))**0.5/screen_size
+    except ZeroDivisionError:
+        pass
     query = pd.DataFrame(np.array([[company, type, ram, weight, touchscreen, ips, ppi , cpu, hdd, ssd, gpu, os]]), columns=['Company', 'TypeName', 'Ram', 'Weight', 'Touchscreen', 'Ips', 'ppi', 'Cpu brand', 'HDD', 'SSD', 'Gpu brand', 'os'])
 
     st.title("The predicted price of this configuration is " + str(int(np.exp(pipe.predict(query)[0]))))
